0337c096e4c707126e0e3b90cbe5ea9bfece95ff
Shawn19702 <shawnboodhoo5@gmail.com>
2022-11-09 21:00:52 -0500

    Bank

--- src/main/java/com/banking/banking/BankingApplication.java
+++ src/main/java/com/banking/banking/BankingApplication.java
@@ -8,6 +8,11 @@ public class BankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplication.class, args);
+//git add -A
+// git commit -m("message of what you have done)
+//git push
+
+//ALWAYS SYNC YOUR FORK BEFORE MAKING CHANGES
 	}
 
 }

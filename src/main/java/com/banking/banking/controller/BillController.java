347f3459e25ae13515858d24e2a48dd1919546aa
VanDiBandit <brovan45@gmail.com>
2022-11-11 17:02:53 -0500

    Get Bill

--- src/main/java/com/banking/banking/controller/BillController.java
+++ src/main/java/com/banking/banking/controller/BillController.java
@@ -1,11 +1,52 @@
 package com.banking.banking.controller;
 
+import com.banking.banking.model.Bill;
 import com.banking.banking.service.BillService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import com.banking.banking.model.Bill;
+import com.banking.banking.service.BillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 public class BillController {
     @Autowired
     private BillService billService;
+    @GetMapping("/bills/{billId}")
+    public ResponseEntity<?> getBillsById(@PathVariable Long billId) {
+        return billService.getBillById(billId);
+    }
+
+    @GetMapping("/accounts/{accountId}/bills")
+    public ResponseEntity<Iterable<Bill>> getAllBillForAccount() {
+        return billService.getAllBills();
+    }
+    @GetMapping("/customers/{customerId}/bills")
+    public ResponseEntity<Iterable<Bill>> getAllBillForCustomer(){
+        return billService.getAllBills();
+    }
+
+    @PostMapping("/accounts/{accountId}/bills")
+    public void createBill(@Valid @RequestBody Bill bill) {
+        billService.createBill(bill);
+    }
+
+    @PutMapping("/bills/{billId}")
+    public void updateBill(@RequestBody Bill bill, @PathVariable Long billId) {
+        billService.updateBill(bill, billId);
+    }
+
+    @DeleteMapping("/bills/{billId}")
+    public ResponseEntity<?> deleteBill(@PathVariable Long billId) {
+        return billService.deleteBill(billId);
+    }
 }
+
--- src/main/java/com/banking/banking/service/BillService.java
+++ src/main/java/com/banking/banking/service/BillService.java
@@ -1,11 +1,41 @@
 package com.banking.banking.service;
 
+import com.banking.banking.model.Bill;
+import com.banking.banking.model.Deposit;
 import com.banking.banking.repository.BillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BillService {
     @Autowired
     private BillRepository billRepository;
+
+    public ResponseEntity<Iterable<Bill>> getAllBills() {
+        Iterable<Bill> bills = billRepository.findAll();
+        return new ResponseEntity<>(bills, HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> getBillById(Long billId) {
+        Bill bill = billRepository.findById(billId).orElse(null);
+        return new ResponseEntity<>(bill, HttpStatus.OK);
+    }
+
+    /*public void verifyBill(Long billId) {
+        Bill bill= BillRepository.findById(billId).orElse(null);
+    }*/
+    public void createBill(Bill bill) {
+        bill=billRepository.save(bill);
+    }
+    public void updateBill(Bill bill, Long billId) {
+        billRepository.save(bill);
+    }
+
+    public ResponseEntity<?> deleteBill(Long billId) {
+        billRepository.deleteById(billId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
+

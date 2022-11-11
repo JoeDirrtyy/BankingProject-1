c781d7bd7982e09d202451058860fc27dd48e432
Shawn19702 <shawnboodhoo5@gmail.com>
2022-11-10 22:47:16 -0500

    Withdrawal and Deposit

--- src/main/java/com/banking/banking/controller/DepositController.java
+++ src/main/java/com/banking/banking/controller/DepositController.java
@@ -1,11 +1,34 @@
 package com.banking.banking.controller;
 
+import com.banking.banking.model.Deposit;
 import com.banking.banking.service.DepositService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DepositController {
     @Autowired
     private DepositService depositService;
+
+
+    @GetMapping("/accounts/{accountId}/deposits")
+    public ResponseEntity<Iterable<Deposit>> getAlldeposits() {
+        return depositService.getAlldeposits();
+    }
+
+    @PostMapping("/accounts/{accountId}/deposits")
+    public Deposit createDeposit(Deposit deposit) {
+        return depositService.createDeposit(deposit);
+    }
+
+    @GetMapping("/deposits/{depositId}")
+    public ResponseEntity<?> getDepositbyId(@PathVariable Long id) {
+        return depositService.getDepositById(id);
+    }
+
+    @PutMapping("/deposits/{depositId}")
+    public ResponseEntity<?> updateDeposit(Deposit deposit, Long deposit_id) {
+        return depositService.updateDeposit(deposit, deposit_id);
+    }
 }
--- src/main/java/com/banking/banking/service/DepositService.java
+++ src/main/java/com/banking/banking/service/DepositService.java
@@ -1,11 +1,42 @@
 package com.banking.banking.service;
 
+import com.banking.banking.model.Deposit;
 import com.banking.banking.repository.DepositRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DepositService {
     @Autowired
     private DepositRepository depositRepository;
+
+    public Deposit createDeposit(Deposit deposit) {
+        return depositRepository.save(deposit);
+    }
+
+    public ResponseEntity<Iterable<Deposit>> getAlldeposits() {
+        Iterable<Deposit> categories = depositRepository.findAll();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+
+
+    public ResponseEntity<?> updateDeposit(Deposit deposit, Long deposit_id) {
+        depositRepository.save(deposit);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    public ResponseEntity<?> deleteDeposit(Long deposit_id) {
+        depositRepository.deleteById(deposit_id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> getDepositById(Long deposit_id) {
+
+        Deposit p = depositRepository.findById(deposit_id).orElse(null);
+        return new ResponseEntity<>(p, HttpStatus.OK);
+
+    }
 }

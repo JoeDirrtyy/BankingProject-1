8fd48777ce3df24b0b39f26186ba58348785c969
Ktabibian <Kyle.tabibian@yahoo.com>
2022-11-10 22:58:52 -0500

    customer methods

--- src/main/java/com/banking/banking/controller/CustomerController.java
+++ src/main/java/com/banking/banking/controller/CustomerController.java
@@ -1,12 +1,39 @@
 package com.banking.banking.controller;
 
+import com.banking.banking.model.Customer;
 import com.banking.banking.repository.CustomerRepository;
 import com.banking.banking.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CustomerController {
     @Autowired
     private CustomerService customerService;
+
+    @PostMapping("/customers")
+    public void createCustomer (Customer customer){
+        customerService.createCustomer(customer);
+    }
+
+    @GetMapping("/customers")
+    public ResponseEntity<Iterable<Customer>> getAllCustomers(){
+        return customerService.getAllCustomers();
+    }
+
+    @GetMapping("/customers/{id}")
+    public ResponseEntity<?> getCustomerById(@PathVariable Long customerId){
+        return customerService.getCustomerById(customerId);
+    }
+
+    @PutMapping("/customer/{id}")
+    public void updateCustomer(@RequestBody Customer customer, @PathVariable Long customerId){
+        customerService.updateCustomer(customer, customerId);
+    }
+
+    @DeleteMapping("/customer/{id}")
+    public ResponseEntity<?> deleteCustomerById(@PathVariable Long customerId){
+        return customerService.deleteCustomerById(customerId);
+    }
 }
--- src/main/java/com/banking/banking/service/CustomerService.java
+++ src/main/java/com/banking/banking/service/CustomerService.java
@@ -1,11 +1,38 @@
 package com.banking.banking.service;
 
+import com.banking.banking.model.Customer;
 import com.banking.banking.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+
     @Autowired
     private CustomerRepository customerRepository;
+
+    public void createCustomer (Customer customer){
+        customerRepository.save(customer);
+    }
+
+    public ResponseEntity<Iterable<Customer>> getAllCustomers(){
+        Iterable<Customer> customers = customerRepository.findAll();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> getCustomerById(Long customerId){
+        Customer customer = customerRepository.findById(customerId).orElse(null);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    public void updateCustomer(Customer customer, Long customerId){
+        Customer c = customerRepository.save(customer);
+    }
+
+    public ResponseEntity<?> deleteCustomerById(Long customerId){
+        customerRepository.deleteById(customerId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
